9efad4a689220273f0cf9323d6aeb0c3731737ac
rainer <rainer.telesko@fhnw.ch>
2020-01-07 09:41:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lotterytoolbarstart/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.lotterytoolbarstart;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Getting the Toolbar
+        Toolbar toolbar = findViewById(R.id.tbExample);
+
+        // Setting the title
+        toolbar.setTitle("Toolbar Start");
+
+        // Placing Toolbar instead of Actionbar
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.mnuShare:
+                Toast.makeText(this, "You clicked 'Share Content'", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mnuVideo:
+                Toast.makeText(this, "You clicked 'Youtube Video'", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.mnuQuit:
+                Toast.makeText(this, "You clicked 'Quit'", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+
+        }
+        return true;
+    }
+}
